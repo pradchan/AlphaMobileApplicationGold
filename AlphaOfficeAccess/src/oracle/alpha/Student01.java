584d6ae199c13192f13f325818b2502580b2bd10
oracle <oracle@localhost>
2014-12-05 11:39:14 -0800

    Modified firstName and lastName variables

--- AlphaOfficeAccess/src/oracle/alpha/Student01.java
+++ AlphaOfficeAccess/src/oracle/alpha/Student01.java
@@ -16,8 +16,8 @@ public class Student01 {
     @Produces(MediaType.APPLICATION_JSON)
     public String sayHello() {
         String message;
-        String firstName = "John";
-        String lastName = "Smith";
+        String firstName = "Dennis";
+        String lastName = "Foley";
         
         message = new String (firstName + " " + lastName + " - You have deployed a REST api to the Oracle Public Cloud");
         return message;
