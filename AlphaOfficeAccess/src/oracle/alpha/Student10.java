2acdbb884b07f16039c401a0ba5bbd5ff373533e
oracle <oracle@localhost>
2015-01-06 21:07:07 -0800

    Reset firstName and lastName

--- AlphaOfficeAccess/src/oracle/alpha/Student10.java
+++ AlphaOfficeAccess/src/oracle/alpha/Student10.java
@@ -17,8 +17,8 @@ public class Student10 {
     
     public String sayHello() {
         String message;
-        String firstName = "Sanjay";
-        String lastName = "Bansal";
+        String firstName = "John";
+        String lastName = "Smith";
         
         message = new String (firstName + " " + lastName + " - You have deployed a REST api to the Oracle Public Cloud");
         return message;
