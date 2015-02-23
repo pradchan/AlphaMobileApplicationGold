dbcee522ef00b94f446fbe9bbff8e320b4946b63
ebarberi <ebarberi@EBARBERI-LAP.us.oracle.com>
2015-02-23 18:11:56 -0500

    Modified first and last name variables for student 04.

--- AlphaOfficeAccess/src/oracle/alpha/Student04.java
+++ AlphaOfficeAccess/src/oracle/alpha/Student04.java
@@ -1,3 +1,5 @@
+// Student04.java
+
 package oracle.alpha;
 
 import javax.ws.rs.GET;
@@ -7,6 +9,9 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/AlphaOfficeAccess/Student04")
 public class Student04 {
+    /**
+     * Default constructor.
+     */
     public Student04() {
         super();
     }
@@ -16,11 +21,10 @@ public class Student04 {
     @Produces(MediaType.APPLICATION_JSON)
     public String sayHello() {
         String message;
-        String firstName = "John";
-        String lastName = "Smith";
-        
+        String firstName = "Danny";
+        String lastName = "Clibon";
+
         message = new String (firstName + " " + lastName + " - You have deployed a REST api to the Oracle Public Cloud");
         return message;
     }
-
 }
