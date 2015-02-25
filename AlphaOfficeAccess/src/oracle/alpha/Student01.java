862df7dcbb29fc85361769400385648b3314528d
oracle <oracle@localhost>
2015-02-25 07:14:07 -0800

    Modified first and last name - MTO 2-25-15

--- AlphaOfficeAccess/src/oracle/alpha/Student01.java
+++ AlphaOfficeAccess/src/oracle/alpha/Student01.java
@@ -16,8 +16,8 @@ public class Student01 {
     @Produces(MediaType.APPLICATION_JSON)
     public String sayHello() {
         String message;
-        String firstName = "Jm";
-        String lastName = "Digne";
+        String firstName = "Matt";
+        String lastName = "Orsie";
         
         message = new String (firstName  + " " + lastName + " - You have deployed a REST api to the Oracle Public Cloud");
         return message;
