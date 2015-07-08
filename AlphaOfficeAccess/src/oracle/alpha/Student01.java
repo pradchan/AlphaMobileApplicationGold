9dd62b56de137be4a09f0a99e34c2576b49a419f
cosmindev <cosmin.tudor82@gmail.com>
2015-07-08 07:31:01 -0700

    Modified firstName and lastName variables

--- AlphaOfficeAccess/src/oracle/alpha/Student01.java
+++ AlphaOfficeAccess/src/oracle/alpha/Student01.java
@@ -17,8 +17,8 @@ public class Student01 {
 
     public String sayHello() {
         String message;
-        String firstName = "Cosmin1";
-        String lastName = "Tudor1";
+        String firstName = "Antonio";
+        String lastName = "Bertoldi";
         
         message = new String (firstName + " " + lastName + " - You have deployed a REST api to the Oracle Public Cloud");
         return message;
