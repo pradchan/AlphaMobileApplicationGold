ea97d227f3cc5635453e0e346d63ed7d2e7976e3
cosmindev <cosmin.tudor82@gmail.com>
2015-08-04 09:08:48 -0700

    add new StudentXX class

---
+++ AlphaOfficeAccess/src/oracle/alpha/Student31.java
@@ -0,0 +1,27 @@
+package oracle.alpha;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/AlphaOfficeAccess/Student01")
+public class Student31 {
+    public Student31() {
+        super();
+    }
+
+    @GET
+    @Path("/sayHello")
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public String sayHello() {
+        String message;
+        String firstName = "Cosmin";
+        String lastName = "Tudor";
+        
+        message = new String (firstName + "" + lastName + " - You have deployed a REST api to the Oracle Public Cloud");
+        return message;
+    }
+}
