7e52d884aeea1fbcce527bc45a69ffb660a2be12
cosmindev <cosmin.tudor82@gmail.com>
2015-08-17 07:35:47 -0700

    Modified firstName and lastName variables

---
+++ AlphaOfficeAccess/src/oracle/alpha/StudentXX.java
@@ -0,0 +1,27 @@
+package oracle.alpha;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/AlphaOfficeAccess/StudentXX")
+public class StudentXX {
+    public StudentXX() {
+        super();
+    }
+
+    @GET
+    @Path("/sayHello")
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public String sayHello() {
+        String message;
+        String firstName = "Dennis";
+        String lastName = "Foley";
+        
+        message = new String (firstName + "" + lastName + " - You have deployed a REST api to the Oracle Public Cloud");
+        return message;
+    }
+}
