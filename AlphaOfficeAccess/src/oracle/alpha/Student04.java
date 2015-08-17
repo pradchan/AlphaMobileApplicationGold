d9bcebd516c567d23f8df8a5db2598aa5f654a17
cosmindev <cosmin.tudor82@gmail.com>
2015-08-17 07:12:11 -0700

    Modified firtst and last name

--- AlphaOfficeAccess/src/oracle/alpha/Student04.java
+++ AlphaOfficeAccess/src/oracle/alpha/Student04.java
@@ -17,8 +17,8 @@ public class Student04 {
 
     public String sayHello() {
         String message;
-        String firstName = "John";
-        String lastName = "Smit";
+        String firstName = "Alexandre";
+        String lastName = "Vieira";
 
         message = new String (firstName + " " + lastName + " - You have deployed a REST api to the Oracle Public Cloud");
         return message;
